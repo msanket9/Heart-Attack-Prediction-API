d033e87db7a64ea653a345dbb2167a89e83bb7c3
SairajK19 <saikapdi11@gmail.com>
2021-04-30 16:28:26 +0530

    Minor Change

--- app.py
+++ app.py
@@ -7,21 +7,46 @@ app = Flask(__name__)
 CORS(app)
 model = pickle.load(open('modelfinal.pkl', 'rb'))
 
-@app.route('/<int:age>/<int:cholestrol>/<int:systolic_pressure>/<int:diastolic_pressure>/<int:bmi>/<int:heart_rate>/<int:glucose>')
-def api_pred(age,cholestrol,systolic_pressure,diastolic_pressure,bmi,heart_rate,glucose):
-    data=np.array([[int(age),int(cholestrol),int(systolic_pressure),int(diastolic_pressure),int(bmi),int(heart_rate),int(glucose)]])
+@app.route('/predict-heart-attack', methods=['GET', 'POST'])
+def api_pred():
+    inputs = request.json
+    data_array = []
+    result = dict()
+    try:
+        for input in inputs:
+            data_array.append(float(inputs[input]["value"]))
 
-    prediction=model.predict(data)
-	
-    result={
-            'age':age,
-            'cholestrol':cholestrol,
-            'systolic_pressure':systolic_pressure,
-            'diastolic_pressure':diastolic_pressure,
-            'bmi':bmi,
-            'heart_rate':heart_rate,
-            'glucose':glucose,
-            'prediction_result':bool(prediction[0])
+        # if any of the input is undefined return failure.
+        if len(data_array) < 7:
+            # return faliure result with the provided data.
+            result = {item: inputs[item]["value"] for item in inputs}
+
+            result["success"] = False
+            result["prediction_result"] = None
+
+            return jsonify(result)
+
+    except:
+        result["success"] = False
+        result["prediction_result"] = None
+        result["message"] = "Invalid Input"
+
+        return jsonify(result)
+
+    data = np.array([data_array])
+
+    prediction = model.predict(data)
+
+    result = {
+        'age': inputs['age']['value'],
+        'cholestrol': inputs['cholestrol']['value'],
+        'systolic_pressure': inputs['systolicPressure']['value'],
+        'diastolic_pressure': inputs['diastolicPressure']['value'],
+        'bmi': inputs['bmi']['value'],
+        'heart_rate': inputs['heartRate']['value'],
+        'glucose': inputs['glucoseConc']['value'],
+        'prediction_result': bool(prediction[0]),
+        'success': True
     }
 
     return jsonify(result)
