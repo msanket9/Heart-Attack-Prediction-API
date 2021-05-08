78984a06010f737cfa7c881afe58cd0cfbbf34fa
Sanket Marathe <sanketmarathe80@gmail.com>
2021-05-08 14:09:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,25 @@ def api_pred():
 
     return jsonify(result)
 
+@app.route('/<int:age>/<int:cholestrol>/<int:systolic_pressure>/<int:diastolic_pressure>/<int:bmi>/<int:heart_rate>/<int:glucose>')
+def api_predu(age,cholestrol,systolic_pressure,diastolic_pressure,bmi,heart_rate,glucose):
+    data=np.array([[int(age),int(cholestrol),int(systolic_pressure),int(diastolic_pressure),int(bmi),int(heart_rate),int(glucose)]])
+
+    prediction=model.predict(data)
+	
+    result={
+            'age':age,
+            'cholestrol':cholestrol,
+            'systolic_pressure':systolic_pressure,
+            'diastolic_pressure':diastolic_pressure,
+            'bmi':bmi,
+            'heart_rate':heart_rate,
+            'glucose':glucose,
+            'prediction_result':bool(prediction[0])
+    }
+
+    return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
